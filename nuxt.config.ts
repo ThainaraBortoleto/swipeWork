f6183029f3c1f25e8e49fb722dc357b9fe0c477f
thainara bortoleto <thainara.bortoleto@gicbrasil.com>
2026-06-28 00:08:59 -0300

    chore: setup inicial Nuxt 3 + TypeScript + Tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+  ],
+  future: {
+    compatibilityVersion: 4,
+  },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: '',
+      supabaseKey: '',
+    },
+  },
+  imports: {
+    dirs: [
+      'composables/**',
+    ],
+  },
+})
